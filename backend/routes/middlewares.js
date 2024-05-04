e23579308b6c34463fb17c4d02ff2b33033258f0
Sandeep <sandeepgunde1@gmail.com>
2024-05-04 22:30:44 +0530

    Added Feature of Update User and MiddleWares

---
+++ backend/routes/middlewares.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const jwt_secret = require("../config");
+function checkSigninCredentials(req,res,next)
+{
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
+    console.log(token);
+    if(!authHeader && !authHeader.startsWith('Bearer '))
+    {
+        res.status(403).json({message:""});
+    }
+    else
+    {
+        try
+        {
+            
+            const decode = jwt.verify(token,jwt_secret)
+            if(decode)
+            {
+                next();
+            }
+        }
+        catch(err)
+        {
+            res.send(403).json({message:"cannot login"});
+        }
+    }
+    const verifying_token = authHeader.split(' ')[1];
+
+    
+}
+
+
+module.exports = checkSigninCredentials;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { User } = require("../db");
 const zod = require("zod");
 const jwt = require("jsonwebtoken");
+const checkSigninCredentials = require("./middlewares");
 const signupBody = zod.object(
     {
         username: zod.string().email(),
@@ -16,14 +17,14 @@ router.post("/signup",async (req,res)=>{
     const {success} = await signupBody.safeParse(req.body);
     if(!success)
     {
-        return res.status(401).json({msg:"Invalid credentials"});
+        return res.status(401).json({message:"Invalid credentials"});
     }
     try{
 
         const dataFound = await User.findOne({username:req.body.username});
         if(dataFound)
         {
-            res.status(404).json({msg:"Account Already Exist with this email"});
+            res.status(404).json({message:"Email already taken / Incorrect inputs"});
         }
         else
         {
@@ -38,7 +39,7 @@ router.post("/signup",async (req,res)=>{
             const token = jwt.sign({username:req.body.username},jwt_secret);
 
             res.status(201).json({
-            msg:"Logged in Succesfully",
+            messsage:"User Created Successfully",
             token:token
             }) 
         }
@@ -55,19 +56,56 @@ router.post("/signup",async (req,res)=>{
 
 router.post("/signin",async(req,res)=>{
     try{
-
         const exist = await User.findOne({username:req.body.username});
-        if(exist)
+        if(!exist && !decode)
         {
-            res.status(200).json({msg:"User found"});
+            res.status(400).json("user not found");  
         }
-        res.status(400).json("user not found");
-       
+        else
+        {
+            res.status(201).json("You are logged In");
+        }
     }
     catch(err)
     {
-        res.status(501).json({msg:"cannot signin"});
+        res.status(441).json({message:"Error while Login"});
     }
 })
+
+
+router.post("/updateUser",checkSigninCredentials,(req,res)=>{
+    user_name = req.body.username;
+    user_firstname = req.body.firstName;
+    user_lastname = req.body.lastName;
+    user_password = req.body.password;
+
+    User.findOneAndUpdate(
+            {username:user_name},
+            {$set:{firstName:user_firstname,lastName:user_lastname,password:user_password,}} 
+    )
+    .then(
+        updateUser=>{
+            if(updateUser)
+            {
+                res.status(200).json({messgae:"User Updated Succesfully"});
+            }
+            else
+            {
+                res.status(404).json({messgae:"User not Found"});
+            }
+
+        }
+    ).catch(err=>{
+        res.status(500).json({message:"Internal Server Error"});
+    })
+
+})
+
+// router.post("/bulk",async(req,res)=>{
+//         const find_name = req.query.filter;
+//         const userExist = await User.findOne({username:find_name});
+//         console.log(userExist);
+
+// })
 
 module.exports = router;
