9dda7122872b4482fd4de44efd3a5194c5c0e925
Sandeep <sandeepgunde1@gmail.com>
2024-05-08 16:47:18 +0530

    Transaction Feature Fixed

--- backend/routes/userAccounts.js
+++ backend/routes/userAccounts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const checkSigninCredentials = require("./middlewares");
 const { User, Account } = require("../db");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 router.post("/balance", checkSigninCredentials, async (req, res) => {
@@ -11,8 +12,7 @@ router.post("/balance", checkSigninCredentials, async (req, res) => {
             return res.status(404).json({ error: "User not found" });
         }
         const data_found = await Account.findOne({userId:user._id});
-        console.log(data_found.balance);
-        res.status(201).json(data_found.balance);
+        res.status(201).json({"message":data_found.balance});
     }
     catch(err)
     {
@@ -22,37 +22,67 @@ router.post("/balance", checkSigninCredentials, async (req, res) => {
 
 
 
-router.post('/transfer', async (req, res) => {
-   const session = await mongoose.startSession();
-
+router.post('/transfer',checkSigninCredentials,async (req, res) => {
+    const session =await mongoose.startSession();
+    const {to,amount} = req.body;
     session.startTransaction();
-    const { amount, to } = req.body;
+    try
+    {
+        // Sender Details
+        const sender_username = req.userId.username;
+        const sender_username_all_data = await User.findOne({username:sender_username}).session(session);
+        if(!sender_username_all_data)
+        {
+            res.status(404).json({message:"Sender not found"})
+        }
+        const sender_account_details = await Account.findOne({userId:sender_username_all_data._id}).session(session); 
+        if(!sender_account_details)
+        {
+            res.status(404).json({message:"Sender Account details not found"});
+        }
+        const sender_account_balance = sender_account_details.balance;
+        
+        // Reciever Details
+        const reciever_user = await User.findOne({username:to}).session(session);
+        if(!reciever_user)
+        {
+            res.status(404).json({message:"reciever details not found"})
+        }
+        const reciver_user_Id = reciever_user._id;
+        const receiver_Account_details = await Account.findOne({userId:reciver_user_Id}).session(session);
+        if(!receiver_Account_details)
+        {
+            res.status(404).json({message:"Reciver account details not found"});
+        }
 
-    const account = await Account.findOne({ userId: req.userId }).session(session);
+        // Transfer Details
+        if(sender_account_balance>=amount)
+        {
+            sender_account_details.balance -= amount;
+            receiver_Account_details.balance += amount;
+            await sender_account_details.save();
+            await receiver_Account_details.save();
+        }
+        else
+        {
+            await session.abortTransaction();
+            res.status(404).json({message:"Your Balance is low"});
+        }
 
-    if (!account || account.balance < amount) {
+        await await session.commitTransaction();
+        // sender_account_details.save();
+        res.status(201).json({message:"Transfer Completed Successfully"});
+    }
+    catch(err)
+    {   
         await session.abortTransaction();
-        return res.status(400).json({
-            message: "Insufficient balance"
-        });
+        res.status(404).json({message:"Transfer Failed"});
+    }
+    finally
+    {
+        await session.endSession();
     }
 
-    const toAccount = await Account.findOne({ userId: to }).session(session);
-
-    if (!toAccount) {
-        await session.abortTransaction();
-        return res.status(400).json({
-            message: "Invalid account"
-        });
-    }
-
-    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
-    await session.commitTransaction();
-    res.json({
-        message: "Transfer successful"
-    });
 });
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Dashboard, Send, Signin, Signup } from "./components/Home"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-        Hello world
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<Signin/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/send" element={<Send/>}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
